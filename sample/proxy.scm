da7ccccb65d3acf85a25603b4fc0a942638e700b
Kouhei Sutou <kou@cozmixng.org>
2004-06-29 07:27:30 +0000

    * Started writing document.

--- sample/proxy.scm
+++ sample/proxy.scm
@@ -15,4 +15,15 @@
     (tuple-space-write tuple-space '(1 2 3) 10000000)
     (tuple-space-write tuple-space '(1 2 3) 10000000)
     (print (tuple-space-read tuple-space '((1 _ _))))
-    (print (tuple-space-read-all tuple-space '((1 _ _))))))
+    (print (tuple-space-read-all tuple-space '((1 _ _))))
+    
+    (tuple-space-write tuple-space '(tag (1 2 3)))
+    (print (cdr (tuple-space-take tuple-space
+                                  '((taaaag ((? number?) ...))))))
+    (print (tuple-space-take tuple-space
+                             '(('taaaag ((? number?) ...)))
+                             1000
+                             #f))
+    (tuple-space-write tuple-space '(tag (1 2 3)))
+    (print (cdr (tuple-space-take tuple-space
+                                  '(('tag ((? number?) ...))))))))
